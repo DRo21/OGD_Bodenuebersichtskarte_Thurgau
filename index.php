c96fc294aa38ff98d77f9eae50745ac8b3464ecb
MFdesigns <michel.faeh@stafag.ch>
2020-09-09 07:53:26 +0200

    Added basic HTML page

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OGD Bodenübersicht</title>
+    <meta name="author" content="Elias Baumgartner, Dario Romandini, Maximilian Hubrath, Michel Fäh">
+    <meta name="description" content="Kartendaten über die Bodenbeschaffenheit im Thurgau">
+</head>
+<body>
+
+    <header>
+        <h1>OGD Bodenübersicht</h1>
+    </header>
+
+    <main>
+    </main>
+
+    <footer>
+        <p>Copyright &copy <?php echo date("Y"); ?> Elias Baumgartner, Dario Romandini, Maximilian Hubrath, Michel Fäh</p>
+    </footer>
+    
+</body>
+</html>
