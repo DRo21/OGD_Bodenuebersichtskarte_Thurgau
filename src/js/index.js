4ebaccc7380e3409aab325c8e0f420d1af53a148
MFdesigns <michel.faeh@stafag.ch>
2020-09-09 20:34:33 +0200

    Added OGD layer with default layer

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,22 @@
-let map = L.map('map', {
-  center: [47.574015,9.106968],
+/*
+  Copyright (c) 2020 Dario Romandini, Michel Fäh
+*/
+
+const ACCESS_KEY = 'YoW2syIQ4xe0ccJA';
+const API_URL = `https://MAP.geo.tg.ch/proxy/geofy_chsdi3/bodenuebersicht-profile_bohrungen?access_key=${ACCESS_KEY}&`;
+
+const MAP = L.map('map', {
+  center: [47.55, 9.05],
   zoom: 10
 });
 
-var wmsLayer = L.tileLayer.wms('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-    opacity: 0.5
-}).addTo(map);
+const OSM_LAYER = L.tileLayer.wms('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMAP</a> contributors',
+  opacity: 0.5
+}).addTo(MAP);
+
+const OGD_LAYER = L.tileLayer.wms(API_URL, {
+  layers: 'Hauptboden',
+  transparent: true,
+  format: 'image/png',
+}).addTo(MAP);
