795a94ddfaf924f43e25c2c62dc9008c9c4d843d
MFdesigns <michel.faeh@stafag.ch>
2020-09-17 20:37:51 +0200

    Added layer display names

--- src/js/index.js
+++ src/js/index.js
@@ -4,18 +4,57 @@
 
 const ACCESS_KEY = 'YoW2syIQ4xe0ccJA';
 const API_URL = `https://map.geo.tg.ch/proxy/geofy_chsdi3/bodenuebersicht-profile_bohrungen?access_key=${ACCESS_KEY}`;
-const LAYER_NAMES = [
-  'Hauptboden',
-  'ubuflach_k_ob_al',
-  'ubuflach_hb',
-  'ubuflach_verd',
-  'Nebenboden',
-  'ubuflach_nb',
-  'Begleitboden',
-  'ubuflach_bb',
-  'ubuflach_re_nat',
-  'ubufixpk',
+const LAYERS = [
+  {
+    name: 'Hauptboden',
+    displayName: 'Hauptboden',
+  },
+  {
+    name: 'ubuflach_bt_al',
+    displayName: 'Bodentyp Hauptboden Landwirtschaft',
+  },
+  {
+    name: 'ubuflach_k_ob_al',
+    displayName: 'Textur Hauptboden Landwirtschaft',
+  },
+  {
+    name: 'ubuflach_hb',
+    displayName: 'Wasserhaushalt Hauptboden',
+  },
+  {
+    name: 'ubuflach_verd',
+    displayName: 'Verdichtungsempfindlichkeit',
+  },
+  {
+    name: 'Nebenboden',
+    displayName: 'Nebenboden',
+  },
+  {
+    name: 'ubuflach_nb',
+    displayName: 'Wasserhaushalt Nebenboden',
+  },
+  {
+    name: 'Begleitboden',
+    displayName: 'Begleitboden',
+  },
+  {
+    name: 'ubuflach_bb',
+    displayName: 'Wasserhaushalt Begleitboden',
+  },
+  {
+    name: 'ubuflach_re_nat',
+    displayName: 'Bodenregionen (natürlich)',
+  },
+  {
+    name: 'ubuflach_re_ant',
+    displayName: 'Bodenregionen (anthropogen)',
+  },
+  {
+    name: 'ubufixpk',
+    displayName: 'Bodenprofile und Bohrungen',
+  },
 ];
+let selectedLayer = LAYERS[0].name; // Default selected layer
 
 // eslint-disable-next-line no-undef
 const MAP = L.map('map', {
@@ -25,27 +64,16 @@ const MAP = L.map('map', {
 
 const LAYER_SELECT = document.getElementById('layer-select');
 
-// Data structure used to track which layer is currently displayed
-// Do not modify outside buildStateStruct function
-// KEY: Layer name
-// Attrib visible: Boolean value if layer is displayed (true) or not (false)
-// Attrib wms: reference to leaflet WMS object
-let selectedLayer = LAYER_NAMES[0]; // Default selected layer
-const LAYERS = {};
-
 /**
- * Initialise layers object
+ * Initialise wms layers
  */
-function buildStateStruct() {
-  LAYER_NAMES.forEach((name) => {
-    LAYERS[name] = {
-      // eslint-disable-next-line no-undef
-      wms: L.tileLayer.wms(API_URL, {
-        layers: name,
-        transparent: true,
-        format: 'image/png',
-      }),
-    };
+function addWMSLinks() {
+  LAYERS.forEach((layer) => {
+    layer.wms = L.tileLayer.wms(API_URL, {
+      layers: layer.name,
+      transparent: true,
+      format: 'image/png',
+    });
   });
 }
 
@@ -53,10 +81,10 @@ function buildStateStruct() {
  * Generate all options in the layer select box
  */
 function generateLayerOptions() {
-  LAYER_NAMES.forEach((name) => {
+  LAYERS.forEach((layer) => {
     const OPT = document.createElement('option');
-    OPT.textContent = name;
-    OPT.value = name;
+    OPT.textContent = layer.displayName;
+    OPT.value = layer.name;
     LAYER_SELECT.appendChild(OPT);
   });
 }
@@ -65,11 +93,11 @@ function generateLayerOptions() {
  * Draw all visible layers
  */
 function drawLayers() {
-  Object.keys(LAYERS).forEach((layerKey) => {
-    if (layerKey === selectedLayer) {
-      LAYERS[layerKey].wms.addTo(MAP);
+  LAYERS.forEach((layer) => {
+    if (layer.name === selectedLayer) {
+      layer.wms.addTo(MAP);
     } else {
-      LAYERS[layerKey].wms.remove();
+      layer.wms.remove();
     }
   });
 }
@@ -93,7 +121,7 @@ document.getElementById('slider').addEventListener('change', () => {
   // eslint-disable-next-line no-undef
   L.tileLayer.wms('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {}).addTo(MAP);
 
-  buildStateStruct();
+  addWMSLinks();
   generateLayerOptions();
   drawLayers();
 }());
